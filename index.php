5a5cb8c3dc76ce04582a199298610c3135795eb1
mbotonga <folong201@gmail.com>
2023-07-13 14:32:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <?php
+    include('nav.php');
+    ?>
+    <div class="container">
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">nom</th>
+                    <th scope="col">prix</th>
+                    <th scope="col">Qty</th>
+                    <th scope="col">categorie</th>
+                    <th scope="col">Detais</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>prix</td>
+                    <td>Qty</td>
+                    <td>categorie</td>
+                    <td>
+                        <div class="btn btn-primary">Show</div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>prix</td>
+                    <td>Qty</td>
+                    <td>categorie</td>
+                    <td>
+                        <div class="btn btn-primary">Show</div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>prix</td>
+                    <td>Qty</td>
+                    <td>categorie</td>
+                    <td>
+                        <div class="btn btn-primary">Show</div>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>prix</td>
+                    <td>Qty</td>
+                    <td>categorie</td>
+                    <td>
+                        <div class="btn btn-primary">Show</div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,25 @@
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Articles</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="creation.php">create</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="udpate.php">update</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="details.php">details</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
